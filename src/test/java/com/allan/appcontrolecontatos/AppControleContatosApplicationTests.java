12bf88b0ef42d7961d99ec08803f4c8e927fb282
AllanYada <allan.yada02@hotmail.com>
2023-11-01 17:15:25 -0300

    Projeto Criado e Organização de dependencias no pom.xml

---
+++ src/test/java/com/allan/appcontrolecontatos/AppControleContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allan.appcontrolecontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppControleContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
